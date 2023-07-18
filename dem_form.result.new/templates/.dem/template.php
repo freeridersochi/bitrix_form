c05599ba65858c933d3c92ba9dde0525e97e5494
freeridersochi <demitryvoit@gmail.com>
2023-07-18 14:53:57 -0300

    prepear new files

--- dem_form.result.new/templates/.dem/template.php
+++ dem_form.result.new/templates/.dem/template.php
@@ -1,6 +1,6 @@
-<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true):?>
-    die();
-<?endif;?>
+<? 
+//$this->addExternalCss("/local/components/d_form.result.new_fifth/templates/.dem/css/common.css");
+?>
 
 <div class="contact-form">
     <!-- Form title -->
@@ -12,86 +12,74 @@
     </div>
     <!-- Form body -->
     <form class="contact-form__form" action="" method="POST">
+        <!-- Name -->
         <div class="contact-form__form-inputs">
-        <? foreach ($arResult["QUESTIONS"] as $FIELD_SID => $arQuestion) :?>
-            <? if ($arQuestion["CAPTION"] == 'name'): ?>
-                <!-- Name -->
-                <div class="input contact-form__input">
-                    <label class="input__label" for="medicine_name">
-                        <div class="input__label-text">Ваше имя*</div>
-                        <input class="input__input" type="text" 
-                                id="medicine_name" 
-                                name="form_text_<?=$arQuestion["STRUCTURE"][0]["ID"];?>" 
-                                value=""
-                                required="">
-                        <div class="input__notification">Поле должно содержать не менее 3-х символов</div>
-                    </label>
-                </div>
-            <? endif ;?>
-            <? if ($arQuestion["CAPTION"] == 'company'): ?>
-                <!-- Company -->
-                <div class="input contact-form__input">
-                    <label class="input__label" for="medicine_company">
-                        <div class="input__label-text">Компания/Должность*</div>
-                        <input class="input__input" 
-                                type="text" 
-                                id="medicine_company" 
-                                name="form_text_<?=$arQuestion["STRUCTURE"][0]["ID"];?>"
-                                value=""
-                                required="">
-                        <div class="input__notification">Поле должно содержать не менее 3-х символов</div>
-                    </label>
-                </div>
-            <? endif ;?>
-            <? if ($arQuestion["CAPTION"] == 'email'): ?>
-                <!-- Email -->
-                <div class="input contact-form__input">
-                    <label class="input__label" for="medicine_email">
-                    <div class="input__label-text">Email*</div>
+            <div class="input contact-form__input">
+                <label class="input__label" for="medicine_name">
+                <div class="input__label-text">Ваше имя*</div>
+                <input class="input__input" type="text" 
+                        id="medicine_name" 
+                        name="form_text_22" 
+                        value=""
+                        required="">
+                <div class="input__notification">Поле должно содержать не менее 3-х символов</div>
+            </label>
+            </div>
+            <!-- Company -->
+            <div class="input contact-form__input">
+                <label class="input__label" for="medicine_company">
+                    <div class="input__label-text">Компания/Должность*</div>
                     <input class="input__input" 
-                            type="email" 
-                            id="medicine_email" 
-                            name="form_email_24" 
+                            type="text" 
+                            id="medicine_company" 
+                            name="form_text_23"
                             value=""
                             required="">
-                        <div class="input__notification">Неверный формат почты</div>
-                    </label>
-                </div>
-            <? endif ;?>
-            <? if ($arQuestion["CAPTION"] == 'phone'): ?>
-                    <!-- Phone -->
-                    <div class="input contact-form__input">
-                        <label class="input__label" for="medicine_phone">
-                        <div class="input__label-text">Номер телефона*</div>
-                        <input class="input__input"
-                                name="form_text_<?=$arQuestion["STRUCTURE"][0]["ID"];?>"
-                                type="tel" 
-                                id="medicine_phone"
-                                data-inputmask="'mask': '+79999999999', 'clearIncomplete': 'true'"
-                                maxlength="12"
-                                x-autocompletetype="phone-full" 
-                                name="medicine_phone" 
-                                value="" 
-                                required="">
-                        </label>
-                    </div>
-                </div>
-            <? endif ;?>
-            <? if ($arQuestion["CAPTION"] == 'message'): ?>
-                <!-- Textarea -->
-                <div class="contact-form__form-message">
-                    <div class="input"><label class="input__label" for="medicine_message">
-                        <div class="input__label-text">Сообщение</div>
-                        <textarea class="input__input" 
-                                    type="text" 
-                                    id="medicine_message" 
-                                    name="form_textarea_<?=$arQuestion["STRUCTURE"][0]["ID"];?>"
-                                value=""></textarea>
-                        <div class="input__notification"></div>
-                    </label></div>
-                </div>
-            <? endif ;?>
-        <? endforeach ;?>        
+                    <div class="input__notification">Поле должно содержать не менее 3-х символов</div>
+                </label>
+            </div>
+            <!-- Email -->
+            <div class="input contact-form__input">
+                <label class="input__label" for="medicine_email">
+                <div class="input__label-text">Email*</div>
+                <input class="input__input" 
+                        type="email" 
+                        id="medicine_email" 
+                        name="form_email_24" 
+                        value=""
+                       required="">
+                    <div class="input__notification">Неверный формат почты</div>
+                </label>
+            </div>
+            <!-- Phone -->
+            <div class="input contact-form__input">
+                <label class="input__label" for="medicine_phone">
+                <div class="input__label-text">Номер телефона*</div>
+                <input class="input__input"
+                        name="form_text_25"
+                        type="tel" 
+                        id="medicine_phone"
+                        data-inputmask="'mask': '+79999999999', 'clearIncomplete': 'true'"
+                        maxlength="12"
+                        x-autocompletetype="phone-full" 
+                        name="medicine_phone" 
+                        value="" 
+                        required="">
+                </label>
+            </div>
+        </div>
+        <!-- Textarea -->
+        <div class="contact-form__form-message">
+            <div class="input"><label class="input__label" for="medicine_message">
+                <div class="input__label-text">Сообщение</div>
+                <textarea class="input__input" 
+                            type="text" 
+                            id="medicine_message" 
+                            name="form_textarea_26"
+                          value=""></textarea>
+                <div class="input__notification"></div>
+            </label></div>
+        </div>
         <div class="contact-form__bottom">
             <!-- Privancy policy -->
             <div class="contact-form__bottom-policy">Нажимая «Отправить», Вы&nbsp;подтверждаете, что
@@ -109,4 +97,43 @@
         </div>
     </form>
 </div>
+
+
+<?
+//проверяем на наличие значений в массиве $_POST
+if (isset($_POST['form_text_22'])){
+	    
+	    $name = $_POST['form_text_22'];
+	    $company = $_POST['form_text_23'];
+	    $email = $_POST['form_email_24'];
+	    $phone = $_POST['form_text_25'];
+	    $message = $_POST['form_textarea_26'];
+	  
+      	//формируем массив с параметрами 18, 19 и т.д. ID полей
+        $arValues = array (
+    	    "form_text_22" => $name,
+    	    "form_text_23" => $company,
+    	    "form_email_24" => $email,
+    	    "form_text_25" => $phone,
+    	    "form_textarea_26" => $message
+        );
+	//задаем ID нашей формы, можно глянуть в админке
+	$FORM_ID = 6;
+	//подключаем модуль форм, т.к. без него скорей всего будет ошибка
+	CModule::IncludeModule("form");
+	
+	// создадим новый результат
+    if ($RESULT_ID = CFormResult::Add($FORM_ID, $arValues))
+    {
+        echo "Результат #".$RESULT_ID." успешно создан";
+    }
+    else
+    {
+        global $strError;
+        echo $strError;
+    }
+}
+?>
+
+
 
